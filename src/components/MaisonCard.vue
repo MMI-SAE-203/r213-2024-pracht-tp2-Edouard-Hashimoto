920c7b4e6c4ca8c4c8e1f0f39f19c014f0c5386e
Edouard-Hashimoto <edouard.hashimoto@edu.univ-fcomte.fr>
2024-05-03 14:34:39 +0200

    création MaisonCard.vue

---
+++ src/components/MaisonCard.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import type { MaisonRecord } from '@/types'
+
+defineProps<MaisonRecord>()
+</script>
+<template>
+  <div class="maison-card border-2 p-1">
+    <p>{{ adresse }}</p>
+    <p>{{ prix }}</p>
+    <p>{{ favori ? 'oui' : 'non' }}</p>
+    <p>{{ nbChambres }}</p>
+    <p>{{ nbSdb }}</p>
+    <p>{{ nomMaison }}</p>
+    <p>{{ surface }}</p>
+  </div>
+</template>
